4f55f462d58a892bd124fcbd1cac343c8647afb3
mshahmalaki <mshahmalaki@gmail.com>
2020-11-09 01:05:38 +0330

    How to write files

--- 01 Automate File Folder and Terminal Interactions/fileIO.py
+++ 01 Automate File Folder and Terminal Interactions/fileIO.py
@@ -1,6 +1,12 @@
 f = open('inputFile.txt', 'r')
+passFile = open('passFile.txt', 'w')
+failFile = open('failFile.txt', 'w')
 for line in f:
     line_split = line.split()
     if line_split[2] == 'P':
-        print(line)
+        passFile.write(line)
+    else:
+        failFile.write(line)
 f.close()
+passFile.close()
+failFile.close()
